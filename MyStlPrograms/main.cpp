7fd737bd9ec6fe79ec284976259bd0f3ac518154
testuser <testuser@Ganeshs-MacBook-Pro.local>
2016-02-06 10:13:47 +0530

    Initial Commit

---
+++ MyStlPrograms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyStlPrograms
+//
+//  Created by testuser on 06/02/16.
+//  Copyright (c) 2016 Test. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
